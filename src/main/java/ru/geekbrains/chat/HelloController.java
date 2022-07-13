73f443cc8e509adbb814aba590bd7078a2d15848
ZagAlex <zagor85@bk.ru>
2022-07-13 09:32:20 +0300

    ChatMain

--- src/main/java/ru/geekbrains/chat/HelloController.java
+++ src/main/java/ru/geekbrains/chat/HelloController.java
@@ -1,14 +1,19 @@
 package ru.geekbrains.chat;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    void doRandom(ActionEvent event) {
+        System.out.println("11111");
     }
+
+    @FXML
+    void initialize() {
+
+    }
+
 }
